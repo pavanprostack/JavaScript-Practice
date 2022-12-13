9447d858588d523205618fca3ddf59017f61a2f6
pavanprostack <pavanprostack@gmail.com>
2022-12-14 04:52:46 +0530

    plyaergame

---
+++ Task-1/playerGame/game.js
@@ -0,0 +1,47 @@
+// Rock, paper, scissors game.....
+
+let playGame = confirm("Shall we paly Rock, Paper, Scissors Game?");
+
+if (playGame) {
+    let playerChoice = prompt("please enter rock, paper, or scissors.");
+    if (playerChoice) {
+
+        let playerOne = playerChoice.trim().toLowerCase();
+
+        if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
+
+            let computerChoice = Math.floor(Math.random() * 4 + 1);
+            
+            let computer = computerChoice === 1 ? "rock"
+                          : computerChoice === 2 ? "paper"
+                          : "scissors";
+
+            let result =
+                playerOne === computer
+                          ? "Tie game"
+                          : playerOne === "rock" && computer === "paper"
+                          ? `playerOne:${playerOne} \n computer:${computer}
+                           \n computer wins!` 
+                          : playerOne === "paper" && computer === "scissors"
+                          ? `playerOne : ${playerOne} \ncomputer : ${computer}
+                           \n computer wins!`
+                          : playerOne === "scissors" && computer === "rock"
+                          ? `playerOne : ${playerOne} \ncomputer : ${computer}
+                           \n computer wins!`
+                          : `playerOne : ${playerOne} \ncomputer : ${computer}
+                          \n playerOne wins`
+                alert(result);
+
+                let playAgain = confirm("play again")
+                playAgain ? location.reload() : alert("Thanks for playing have nice day...")
+
+        } else {
+            alert("you did n't enter rock, paper, scissors")
+        }
+    } else {
+        alert("I guess you changed your mind. may be next time.")
+    }
+}
+else {
+    alert("Ok, may be next time.")
+}

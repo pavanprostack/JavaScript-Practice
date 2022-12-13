3f63de743174e00069a4d10d6f6a5031bbd49c7a
pavanprostack <pavanprostack@gmail.com>
2022-12-13 08:22:23 +0530

    random

---
+++ mathMethods/random.js
@@ -0,0 +1,27 @@
+// random()
+
+// using this method we can print random number by system
+
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+
+// if we want to print random numbers from 1 to 10
+
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+// if we want to print random numbers from 1 to 4
+
+console.log(Math.floor(Math.random() * 4) + 1)
+console.log(Math.floor(Math.random() * 4) + 1)
+console.log(Math.floor(Math.random() * 4) + 1)
+
+// write a code thet will return a random letter from your name?
+
+let name = "pavankalyan"
+
+console.log(name.charAt(Math.floor(Math.random() * name.length)))
+console.log(name.charAt(Math.floor(Math.random() * name.length)))
+console.log(name.charAt(Math.floor(Math.random() * name.length)))

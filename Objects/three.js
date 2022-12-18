02800b053bb5090dd538b413d7a3d47086dc9979
pavanprostack <pavanprostack@gmail.com>
2022-12-18 11:17:34 +0530

    okk

--- Objects/three.js
+++ Objects/three.js
@@ -12,7 +12,7 @@ console.log(Object.keys(band));  // it returns keys as in Array.
 console.log(Object.values(band))  // it returns key-values as in Array.
 
 for(let job in band){
-    console.log(job)
-    console.log(band[job])
+    console.log(job)   // it prints keys
+    console.log(band[job])  // it print values
     console.log(`On ${job}, its ${band[job]}!`)
 }
